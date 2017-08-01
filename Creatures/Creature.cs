9cdbe045a11fd5b9466fd2bdd77fc0d1240af9c5
zaqwer <platonov256@gmail.com>
2017-08-02 01:59:13 +0800

    Добавлен функционал для позиционирования существа в мире.

--- Creatures/Creature.cs
+++ Creatures/Creature.cs
@@ -1,22 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace DotNetGame.Creatures
 {
     public class Creature : GameObject
     {
         public int Hp { get; private set; }
-
+        private World world;
         private Inventory inventory;
 
-        public Creature(string name, int hp, Inventory inventory) : base(name)
+        //Координаты существа в мире.
+        private int x, y;
+        
+        public Creature(World world, string name, int hp, Inventory inventory) : base(name)
         {
             this.inventory = inventory;
             this.Hp = hp;
+            this.world = world;
         }
-
+        
+        ///Базовое получение урона
         public void takeDamage(int damage)
         {
             Hp -= damage;
+        }
+
+        //Получить локацию, в которой находится существо по его текущим координатам. 
+        public Location getLocation()
+        {
+            return world.getLocation(x, y);
         }
     }
 }
