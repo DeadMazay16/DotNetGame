fd9c34903675fbba90d60cf07df9719d2a306f79
kirill <kirill@kirill.kirill>
2017-08-02 05:14:46 +0800

    Создан класс Weapon

--- Items/Equipment/Equipment.cs
+++ Items/Equipment/Equipment.cs
@@ -6,11 +6,17 @@ namespace DotNetGame.Items.Equipment
     {
         protected string slot;//слот, который занимает этот предмет
         protected IIntelligent owner;//существо-носитель
+        protected string type;//тип данной шмотки
+        protected int quality;//качество данной шмотки
+        protected int level;//уровень шмота, на основе которого считаются бонусы 
         
-        protected Equipment(string name, float weight, int numberOfSlots, string slot, IIntelligent owner) : base(name, weight, numberOfSlots)
+        protected Equipment(string name, float weight, int numberOfSlots, string slot, IIntelligent owner, string type, int quality,int level) : base(name, weight, numberOfSlots)
         {
             this.slot = slot;
             this.owner = owner;
+            this.quality = quality;
+            this.type = type;
+            this.level = level;
         }
 
         //получение баффов и дебаффов при надевании шмотки 
