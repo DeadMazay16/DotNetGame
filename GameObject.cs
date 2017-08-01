27421a56ac40495cd2c88a874478b3648c13b6f5
zaqwer <platonov256@gmail.com>
2017-08-02 00:04:44 +0800

    Описан GameObject

--- GameObject.cs
+++ GameObject.cs
@@ -2,6 +2,15 @@
 {
     public class GameObject
     {
-        //
+        private static int _id = 0;
+        public int Id { get; }
+        public string Name { get; }
+
+        public GameObject(string name)
+        {
+            Id = _id;
+            this.Name = name;
+            _id++;
+        }
     }
 }
