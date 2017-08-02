b5065a149182e7a75b63be32f08ac2bbbec0f571
kirill <kirill@kirill.kirill>
2017-08-02 01:08:25 +0000

    Добавлен класс GameObject

---
+++ Items/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetGame.Items
+{
+    public class Item:GameObject 
+    {
+        private float weight;
+        private int numberOfSlots;
+
+        public Item(string name, float weight, int numberOfSlots) : base(name)
+        {
+            this.weight = weight;
+            this.numberOfSlots = numberOfSlots;
+        }
+    }
+}
