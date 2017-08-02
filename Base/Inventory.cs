21c2ced8468cf63257134a5fc30ec3ded6be75b2
kirill <kirill@kirill.kirill>
2017-08-02 02:03:31 +0000

    Добавлен класс GameObject

---
+++ Base/Inventory.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetGame
+{
+    public class Inventory
+    {
+        public float maxWeight { get; } //максимально возможный переносимый вес
+        public int numberOfSlots { get;  } //количество слотов в инвентаре
+
+        public Inventory(float maxWeight, int numberOfSlots){
+            this.maxWeight = maxWeight;
+            this.numberOfSlots = numberOfSlots;
+        }
+    }
+}
