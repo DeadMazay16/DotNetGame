455a56e7b002c91e1c1966e7ba97b1452c235072
kirill <kirill@kirill.kirill>
2017-08-02 03:04:46 +0000

    Создан класс Equipment

---
+++ Items/Equipment/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using DotNetGame.Interfeses;
+
+namespace DotNetGame.Items.Equipment
+{
+    public abstract class Equipment:Item
+    {
+        protected string slot;//слот, который занимает этот предмет
+        protected IIntelligent owner;//существо-носитель
+        
+        protected Equipment(string name, float weight, int numberOfSlots, string slot, IIntelligent owner) : base(name, weight, numberOfSlots)
+        {
+            this.slot = slot;
+            this.owner = owner;
+        }
+
+        //получение баффов и дебаффов при надевании шмотки 
+        protected abstract void equipd();
+
+        //отключение баффов и дебаффов при снятии шмотки
+        protected abstract void unEquipd();
+    }
+}
